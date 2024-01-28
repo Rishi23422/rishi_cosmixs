95227f90571711a61debca9990695c8480389788
Rishi Pal <52551855+Rishi23422@users.noreply.github.com>
2024-01-28 04:51:08 +0000

    Sample Maven Project

--- JAVA Basics/BasicJavaPrograms/AbstractClass/AbstractClassExample.java
+++ JAVA Basics/BasicJavaPrograms/AbstractClass/AbstractClassExample.java
@@ -1,6 +1,7 @@
 package AbstractClass;
 
 public class AbstractClassExample {
+    
     public static void main(String[] args) {
         Circle circle = new Circle("Red", 5.0);
         circle.display();
